52931bbb9033714384d3a4a971718eac89170059
ilkay <06ilkayozturk@gmail.com>
2023-12-27 21:51:37 +0300

    commit

--- server/database/conn.js
+++ server/database/conn.js
@@ -7,6 +7,7 @@
 import mongoose from "mongoose";
 
 import { MongoMemoryServer } from "mongodb-memory-server";
+import ENV from "../config.js";
 
 async function connect() {
   const mongod = await MongoMemoryServer.create();
